ab463827c1281c1c0ce82334a6385317743e5ec3
Sean DuBois <sean@siobud.com>
2015-10-30 06:29:45 +0000

    Clicking on a breakpoint in the breakpoint list will now jump the Code viewer to the appropriate file+line

--- web/js/views/Breakpoints.js
+++ web/js/views/Breakpoints.js
@@ -7,10 +7,16 @@ var BreakpointsTemplate = require('../../templates/Breakpoints.hbs'),
 module.exports = require('backbone').View.extend({
     events: {
         "click [data-remove-breakpoint]" : "_onRemoveBreakpoint",
+        "click [data-breakpoint]" : "_onShowBreakpoint",
     },
     _onRemoveBreakpoint: function(e) {
         var parentEl = e.currentTarget.parentElement;
         this.webSocket.emitRemoveBreakpoint(parentEl.getAttribute('data-file'), parentEl.getAttribute('data-line'));
+    },
+    _onShowBreakpoint: function(e) {
+        var parentEl = e.currentTarget.parentElement;
+        this.webSocket.emitSetActiveFile(parentEl.getAttribute('data-file'));
+        this.webSocket.emitSetActiveLine(parentEl.getAttribute('data-line'));
     },
 
     initialize: function(args) {
@@ -25,6 +31,6 @@ module.exports = require('backbone').View.extend({
         }
     },
     removeBreakpoint: function(file, line) {
-       this.$el.find('tr[data-file="' + file + '"][data-line="' + line + '"]').remove();
+       this.$el.find('div[data-file="' + file + '"][data-line="' + line + '"]').remove();
     },
 });
