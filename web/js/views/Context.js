f5b97a3ee94994340de934658f021c0251064b70
Sean DuBois <sean@siobud.com>
2015-11-03 15:27:51 +0000

    Improve context value hover implementation

--- web/js/views/Context.js
+++ web/js/views/Context.js
@@ -1,8 +1,9 @@
 require('../../css/Context.css');
 
-var ContextTemplate = require('../../templates/Context.hbs'),
+var ContextTemplate  = require('../../templates/Context.hbs'),
     ContextsTemplate = require('../../templates/Contexts.hbs'),
-    _               = require('lodash');
+    _                = require('lodash'),
+    $                = require('jquery');
 
 
 module.exports = require('backbone').View.extend({
@@ -12,6 +13,21 @@ module.exports = require('backbone').View.extend({
     render: function() {
         this.el.innerHTML = ContextsTemplate();
     },
+    events: {
+        "mouseenter .context-name span": "_onEnterValue",
+        "mouseleave .context-name": "_onLeaveValue"
+    },
+    _onEnterValue: function(e) {
+        var parent = $(e.currentTarget).parent();
+
+        $(e.currentTarget).addClass('bold')
+        parent.find('.context-value').slideDown();
+    },
+    _onLeaveValue: function(e) {
+        $(e.currentTarget).find('span').removeClass('bold')
+        $(e.currentTarget).find('.context-value').slideUp();
+    },
+
     setContext: function(context) {
         var $contexts = this.$el.find('[data-contexts]');
         if (_.isNull(context.depth)) {
